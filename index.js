cd5ddba93999eb9348b4a695ccd580cbdc84be02
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-11-30 00:18:42 +0200

    [chore] change minor heading in html

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html_pages', 'inde
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'html_pages', 'about.html')));
 app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, 'html_pages', 'contact-me.html')));
 
-
 // The path.join() method joins all given path segments together.
 
 app.use((req, res) => res.status(404).sendFile(path.join(__dirname, 'html_pages', '404.html')));
-
 
 //res.status function accepts a single parameter code that holds the HTTP status code. 
 
 
 app.listen(port, () => console.log(`Server is now running on http://localhost:${port}`));
+//the log function prints a message on the terminal when you run node index.js
