f4da3826ae3ee0d2420e572a2a90bdb47ad2d9d5
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-11-30 12:53:51 +0200

    [chore] fix white space

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path')
 const port = 8080;
 
 app.use(express.static('html_pages'));
-
 // express.static(root, [options]) is a built-in Express middleware function 
 // and is designed to serve static files such as my HTML files
 // in html_pages fodler.
@@ -12,14 +11,12 @@ app.use(express.static('html_pages'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html_pages', 'index.html')));
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'html_pages', 'about.html')));
 app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, 'html_pages', 'contact-me.html')));
-
 // The path.join() method joins all given path segments together.
 
 app.use((req, res) => res.status(404).sendFile(path.join(__dirname, 'html_pages', '404.html')));
-
 //res.status function accepts a single parameter code that holds the HTTP status code. 
-// The error message appears if you try to visit any other localhost:8080 path beside the index, about and contact-me html pages.
-
+// The error message appears if you try to visit any other localhost:8080 path
+// beside the index, about and contact-me html pages.
 
 app.listen(port, () => console.log(`Server is now running on http://localhost:${port}`));
 //the log function prints a message on the terminal when you run node index.js
